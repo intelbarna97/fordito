abffb41d9921bfd02ccd7243ac4c0d3723272bbe
Vereb Barna <intelbarna97@gmail.com>
2022-11-17 23:32:53 +0100

    intial commit

---
+++ fordito/Form1.Designer.cs
@@ -0,0 +1,224 @@
+﻿namespace fordito
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.originalLabel = new System.Windows.Forms.Label();
+            this.convertedLabel = new System.Windows.Forms.Label();
+            this.pathLabel = new System.Windows.Forms.Label();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(138, 11);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(449, 22);
+            this.textBox1.TabIndex = 0;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(138, 52);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(449, 22);
+            this.textBox2.TabIndex = 1;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(138, 90);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(449, 22);
+            this.textBox3.TabIndex = 2;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(593, 11);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "Convert";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(593, 51);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 4;
+            this.button2.Text = "Convert";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(593, 89);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 5;
+            this.button3.Text = "Import";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(12, 137);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersWidth = 51;
+            this.dataGridView1.RowTemplate.Height = 24;
+            this.dataGridView1.Size = new System.Drawing.Size(773, 468);
+            this.dataGridView1.TabIndex = 6;
+            // 
+            // originalLabel
+            // 
+            this.originalLabel.AutoSize = true;
+            this.originalLabel.Location = new System.Drawing.Point(12, 14);
+            this.originalLabel.Name = "originalLabel";
+            this.originalLabel.Size = new System.Drawing.Size(84, 16);
+            this.originalLabel.TabIndex = 8;
+            this.originalLabel.Text = "Original Input";
+            // 
+            // convertedLabel
+            // 
+            this.convertedLabel.AutoSize = true;
+            this.convertedLabel.Location = new System.Drawing.Point(12, 54);
+            this.convertedLabel.Name = "convertedLabel";
+            this.convertedLabel.Size = new System.Drawing.Size(100, 16);
+            this.convertedLabel.TabIndex = 9;
+            this.convertedLabel.Text = "Converted Input";
+            // 
+            // pathLabel
+            // 
+            this.pathLabel.AutoSize = true;
+            this.pathLabel.Location = new System.Drawing.Point(12, 92);
+            this.pathLabel.Name = "pathLabel";
+            this.pathLabel.Size = new System.Drawing.Size(34, 16);
+            this.pathLabel.TabIndex = 10;
+            this.pathLabel.Text = "Path";
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(942, 108);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(75, 23);
+            this.button4.TabIndex = 11;
+            this.button4.Text = "Solve";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(674, 89);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(75, 23);
+            this.button5.TabIndex = 12;
+            this.button5.Text = "Export";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 16;
+            this.listBox1.Location = new System.Drawing.Point(791, 137);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(238, 468);
+            this.listBox1.TabIndex = 13;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.Color.White;
+            this.pictureBox1.Location = new System.Drawing.Point(1035, 137);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(875, 468);
+            this.pictureBox1.TabIndex = 14;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Paint += new System.Windows.Forms.PaintEventHandler(this.pictureBox1_Paint);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1922, 625);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.pathLabel);
+            this.Controls.Add(this.convertedLabel);
+            this.Controls.Add(this.originalLabel);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Label originalLabel;
+        private System.Windows.Forms.Label convertedLabel;
+        private System.Windows.Forms.Label pathLabel;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.ComponentModel.BackgroundWorker backgroundWorker1;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+    }
+}
+
---
+++ fordito/Form1.cs
@@ -0,0 +1,364 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Runtime.InteropServices;
+using Excel = Microsoft.Office.Interop.Excel;
+
+namespace fordito
+{
+    public partial class Form1 : Form
+    {
+
+        string path;
+        string[,] data;
+        string input, stack, rules;
+        TreeNode tree;
+        List<string> list;
+        int maxHeight = 0;
+        Graphics g;
+        bool draw = false;
+
+        public Form1()
+        {
+            InitializeComponent();
+            listBox1.Items.Clear();
+        }
+
+        private string converter(string input)
+        {
+            return Regex.Replace(input, "[0-9]+", "i")+"#";
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            textBox2.Text = converter(textBox1.Text);
+            input = textBox2.Text;
+            stack = "E#";
+            rules = "";
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            input = textBox2.Text;
+            stack = "E#";
+            rules = "";
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            path = @textBox3.Text;
+            Cursor.Current = Cursors.WaitCursor;
+
+            Excel.Application xlApp = new Excel.Application();
+            Excel.Workbook xlWorkbook = xlApp.Workbooks.Open(path);
+            Excel._Worksheet xlWorksheet = xlWorkbook.Sheets[1];
+            Excel.Range xlRange = xlWorksheet.UsedRange;
+
+            int rows = xlRange.Rows.Count;
+            int columns = xlRange.Columns.Count;
+
+            data = new string [columns, rows];
+
+            this.dataGridView1.ColumnCount = columns;
+            this.dataGridView1.RowCount = rows;
+
+            for(int i = 1; i <= rows; i++)
+            {
+                for (int j = 1; j <= columns; j++)
+                {
+                    if(xlRange.Cells[i, j].Value != null)
+                    {
+                        this.dataGridView1[j - 1, i-1].Value = xlRange.Cells[i, j].Value2.ToString();
+                        data[j-1, i-1] = xlRange.Cells[i, j].Value2.ToString();
+                    }
+                    else
+                    {
+                        this.dataGridView1[j - 1, i - 1].Value = "";
+                        data[j - 1, i - 1] = "";
+                    }
+                }
+            }
+
+            GC.Collect();
+            GC.WaitForPendingFinalizers();
+
+            Marshal.ReleaseComObject(xlRange);
+            Marshal.ReleaseComObject(xlWorksheet);
+
+            xlWorkbook.Close();
+            Marshal.ReleaseComObject(xlWorkbook);
+
+            xlApp.Quit();
+            Marshal.ReleaseComObject(xlApp);
+
+            Cursor.Current = Cursors.Default;
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            listBox1.Items.Clear();
+            input = textBox2.Text;
+            stack = "E#";
+            rules = "";
+            list = new List<string>();
+            tree = new TreeNode(stack[0].ToString(), null);
+            solver();
+            for(int i = 0; i < list.Count; i++)
+            {
+                listBox1.Items.Add(list[i]);
+            }
+            buildTree(tree, 1);
+            listBox1.Items.Add(maxHeight.ToString());
+            draw = true;
+            pictureBox1.Invalidate();
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            path = @textBox3.Text;
+            Cursor.Current = Cursors.WaitCursor;
+            Excel.Application xlApp = new Excel.Application();
+            Excel.Workbook xlWorkbook = xlApp.Workbooks.Open(path);
+            Excel._Worksheet xlWorksheet = xlWorkbook.Sheets[1];
+            Excel.Range xlRange = xlWorksheet.UsedRange;
+
+
+            for (int i = 1; i <= this.dataGridView1.RowCount; i++)
+            {
+                for (int j = 1; j <= this.dataGridView1.ColumnCount; j++)
+                {
+                    if (this.dataGridView1[j - 1, i - 1].Value != null)
+                    {
+                        xlWorksheet.Cells[i, j] = this.dataGridView1[j - 1, i - 1].Value;
+                    }
+                    else
+                    {
+                        xlWorksheet.Cells[i, j] = "";
+                    }
+                }
+            }
+            xlApp.Visible = false;
+            xlApp.UserControl = false;
+            xlWorkbook.Save();
+
+            GC.Collect();
+            GC.WaitForPendingFinalizers();
+
+            Marshal.ReleaseComObject(xlRange);
+            Marshal.ReleaseComObject(xlWorksheet);
+
+            xlWorkbook.Close();
+            Marshal.ReleaseComObject(xlWorkbook);
+
+            xlApp.Quit();
+            Marshal.ReleaseComObject(xlApp);
+
+            Cursor.Current = Cursors.Default;
+        }
+
+        private void solver()
+        {
+            while(true)
+            {
+                if(dataGridView1.RowCount<2 || dataGridView1.ColumnCount<2)
+                {
+                    MessageBox.Show("Üres táblázat!");
+                    break;
+                }
+
+                if(input=="#" && stack == "#")
+                {
+                    listBox1.Items.Add(styleConvert());
+                    break;
+                }
+
+                int x = 0;
+                string temp;
+
+                if (stack[1].ToString() == "'")
+                {
+                    temp = stack[0].ToString()+stack[1].ToString();
+                }
+                else
+                {
+                    temp = stack[0].ToString();
+                }
+
+                while(input[0].ToString()!=data[x,0])
+                {
+                    x++;
+                }
+
+                int y = 0;
+                while(temp.ToString()!=data[0,y])
+                {
+                    y++;
+                }
+
+                if(data[x,y]=="")
+                {
+                    MessageBox.Show("Hiba!"+x.ToString()+y.ToString());
+                    break;
+                }
+
+                calculateStep(x, y);
+
+                listBox1.Items.Add(styleConvert());
+
+
+            }
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private string styleConvert()
+        {
+            return "(  "+input+",  " + stack+",  " + rules+"  )";
+        }
+
+        private void calculateStep(int x, int y)
+        {
+            string[] step = Regex.Replace(data[x, y], @"[()\s]", "").Split(',');
+            if (stack[1].ToString()=="'")
+            {
+                stack = stack.Substring(2);
+            }
+            else
+            {
+                stack=stack.Substring(1);
+            }
+            if(step[0]=="e")
+            {
+                rules = rules+step[1];
+            }
+            else if(step[0].ToLower()=="pop")
+            {
+                input = input.Substring(1);
+            }
+            else
+            {
+                stack = step[0] + stack;
+                rules = rules + step[1];
+            }
+            list.Add(step[0]);
+        }
+
+        private void buildTree(TreeNode node, int height)
+        {
+            if(list.Count==0)
+                return;
+
+            if(height>maxHeight)
+            {
+                maxHeight = height;
+            }
+            string value, word = list[0];
+            list.RemoveAt(0);
+            if(word.ToLower()=="pop")
+            {
+                return;
+            }
+
+            for(int i=0;i< word.Length;i++)
+            {
+                if(i< word.Length-1 && word[i+1].ToString()=="'")
+                {
+                    value = word[i].ToString()+ word[i+1].ToString();
+                    i++;
+                }
+                else
+                {
+                    value = word[i].ToString();
+                }
+
+                if(node.left==null)
+                {
+                    node.left = new TreeNode(value, node);
+                    buildTree(node.left, height+1);
+                }
+                else if(node.middle==null)
+                {
+                    node.middle = new TreeNode(value, node);
+                    buildTree(node.middle, height+1);
+                }
+                else if(node.right==null)
+                {
+                    node.right = new TreeNode(value, node);
+                    buildTree(node.right,height+1);
+                }
+            }
+
+        }
+
+        private void pictureBox1_Paint(object sender, PaintEventArgs e)
+        {
+            g = e.Graphics;
+            if(draw)
+            {
+                drawGraph(g, tree, 1, 1, 1, new PointF(0, 0));
+                MessageBox.Show("rajz");
+                draw = false;
+            }
+        }
+
+        private void drawGraph(Graphics g,TreeNode node, int height, double width, double number, PointF point)
+        {
+            float incY = pictureBox1.Height / (maxHeight+2);
+            float y = incY*height;
+
+            double incX = (pictureBox1.Width) / (width + 1);
+            float x = (float)(incX * number);
+
+            if (point.X != 0 && point.Y!=0)
+            {
+                g.DrawLine(new Pen(Color.Black), point, new PointF(x, y));
+                g.DrawEllipse(new Pen(Color.Black), point.X, point.Y, 15, 15);
+                g.DrawString(node.parent.value, new Font("Arial", 10), Brushes.Black, point);
+            }
+
+            g.DrawEllipse(new Pen(Color.Black), x, y, 15, 15);
+            g.DrawString(node.value, new Font("Arial", 10), Brushes.Black, new PointF(x, y));
+
+
+
+
+            if(node.left!=null)
+            {
+                drawGraph(g, node.left, height + 1,width*3, 3*number-2, new PointF(x, y));
+            }
+            if(node.middle!=null)
+            {
+                drawGraph(g, node.middle, height + 1, width * 3, 3*number - 1, new PointF(x, y));
+            }
+            if(node.right!=null)
+            {
+                drawGraph(g, node.right, height + 1, width * 3, 3*number, new PointF(x, y));
+            }
+        }
+
+
+
+        internal class TreeNode
+        {
+            public string value;
+            public TreeNode left, middle, right, parent;
+
+            public TreeNode(string value, TreeNode parent)
+            {
+                this.value = value;
+                this.parent = parent;
+            }
+        }
+
+    }
+}
